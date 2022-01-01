b6191a611173ae8026145b6bf50090aa5c3656de
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-02 01:09:51 +0530

    best setup

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,14 +1,14 @@
 export default {
-    title: "Crypto Check - Get all details about the prices of crypto crrencies",
-    description: "Crypto Check - Get all details about the prices of crypto crrencies",
+    title: "Crypto Check - Get all details about the prices of crypto currencies",
+    description: "Crypto Check - Get all details about the prices of crypto currencies",
     openGraph: {
-        title: "Crypto Check - Get all details about the prices of crypto crrencies",
-        description: "Crypto Check - Get all details about the prices of crypto crrencies",
+        title: "Crypto Check - Get all details about the prices of crypto currencies",
+        description: "Crypto Check - Get all details about the prices of crypto currencies",
         type: 'website',
         locale: 'en_IN',
         images: [
             {
-                url: "https://res.cloudinary.com/sportzhut/image/upload/v1627916090/logo_d80ajt.png",
+                url: "https://res.cloudinary.com/dbxcernxw/image/upload/v1641065965/cryptocurrency_dtwwab.png",
                 width: 300,
                 height: 300,
                 alt: "SportzHut",
---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="icon" href="/favicon.ico"/>
+                    <meta name="theme-color" content="#000"/>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}

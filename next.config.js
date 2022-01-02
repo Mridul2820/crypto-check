47f59ba3758715cd75aa380303c4076ae2d0beee
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-02 13:56:06 +0530

    babel configure

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
+require('dotenv').config()
+
 module.exports = {
-    env: { },
+    env: {},
     publicRuntimeConfig: {},
-    swcMinify: true,
     images: {
         formats: [
             'image/avif', 
@@ -12,5 +14,16 @@ module.exports = {
             'res.cloudinary.com',
             'assets.coingecko.com'
         ]
+    },
+
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['public'] = path.join(__dirname, 'public')
+
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+
+        return config
     },
 }

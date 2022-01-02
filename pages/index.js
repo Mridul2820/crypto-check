b1c9f2e50e49658bf3cae70963b0a8de984789c9
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-03 00:44:58 +0530

    fetch update

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
-// import CoinRow from '../components/CoinRow';
+import CoinRow from '../components/CoinRow';
 
-// export const getServerSideProps = async () => {
-//     const res = await fetch(
-//       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1&price_change_percentage=1h,24h,7d&sparkline=true'
-//     );
+export const getStaticProps = async () => {
+    const res = await fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1&price_change_percentage=1h,24h,7d&sparkline=true'
+    );
   
-//     const filteredCoins = await res.json();
+    const filteredCoins = await res.json();
   
-//     return {
-//         props: {
-//             filteredCoins
-//         }
-//     };
-// };
+    return {
+        props: {
+            filteredCoins
+        },
+        revalidate: 60
+    };
+};
 
-const index = () => {
+const index = ({ filteredCoins }) => {
     return (
         <div className="px-5 flex flex-col pb-4 min-h-[50vh]">
             <div className="grid gap-2 md:gap-4 grid-cols-gridcoinsm md:grid-cols-gridcoin items-center sticky z-10 top-0 px-0 sm:px-3 py-4 text-gray-700 border-b-[1px] border-gray-600 bg-slate-50">
@@ -30,13 +31,13 @@ const index = () => {
                 <span className="hidden md:block">Last 7 Days</span>
             </div>
 
-            {/* {filteredCoins?.map((coin, index) => (
+            {filteredCoins?.map((coin, index) => (
                 <CoinRow 
                     key={coin.id}
                     coin={coin}
                     index={index}
                 />
-            ))} */}
+            ))}
         </div>
     )
 }

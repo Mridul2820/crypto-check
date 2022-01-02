84ca3f493a671c74b13753adb69d7f133e29ff2f
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-02 13:01:14 +0530

    indevidual data fetch for coins

---
+++ pages/coin/[id].js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export async function getServerSideProps(context) {
+    const { id } = context.query;
+  
+    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
+  
+    const data = await res.json();
+  
+    return {
+        props: {
+            coin: data
+        }
+    };
+}
+  
+
+const Coin = ({ coin }) => {
+    return (
+        <div>
+            {coin.name}
+        </div>
+    )
+}
+
+export default Coin

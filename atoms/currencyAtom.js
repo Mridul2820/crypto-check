1b5ab5507943d0d6ae973212fc034c36d081073a
mridul2820 <immridulpanda2820@gmail.com>
2022-01-07 12:49:30 +0530

    currency save in localstorage

--- atoms/currencyAtom.js
+++ atoms/currencyAtom.js
@@ -2,5 +2,5 @@ import { atom } from 'recoil';
 
 export const currencyState = atom({
     key: 'currencyIdState',
-    default: 'inr',
+    default: typeof window !== 'undefined' && localStorage.getItem('currency') || 'inr',
 });

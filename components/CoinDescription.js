93e72aaee79b28d50f0fe8c9be1c0cb7097c7079
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-07 23:11:30 +0530

    build lint fix

--- components/CoinDescription.js
+++ components/CoinDescription.js
@@ -6,9 +6,9 @@ const CoinDescription = ({ name, description }) => {
     return (
         <div className='mt-10 shadow-bs2 max-w-[1000px] mx-auto rounded-md bg-white p-5'>
             <h3 className='font-bold text-2xl mb-4'>What is {name}?</h3>
-            {/* eslint-disable-next-line */}
             <ReactMarkdown 
                 rehypePlugins={[rehypeRaw]} 
+                // eslint-disable-next-line
                 children={description} 
                 className='prose max-w-full text-base'
             />

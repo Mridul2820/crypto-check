41cf8c67f011e3d07f99a98e7d945b0340a6e27a
Mridul2820 <immridulpanda2820@gmail.com>
2022-01-18 19:00:39 +0530

    sitemap and robots.txt create

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,7 +11,7 @@ export default {
                 url: "https://res.cloudinary.com/dbxcernxw/image/upload/v1641915710/app_logos/logo_tpodb9.png",
                 width: 300,
                 height: 300,
-                alt: "SportzHut",
+                alt: "Crypto Check",
             }
         ],
     },
---
+++ next-sitemap.js
@@ -0,0 +1,18 @@
+const { SITE_URL } = process.env
+
+const siteUrl = SITE_URL
+
+module.exports = {
+    siteUrl,
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: "*", disallow: "/500" },
+            { userAgent: "*", allow: "/" },
+        ],
+        additionalSitemaps: [
+            `${siteUrl}/sitemap.xml`,
+        ],
+    },
+    exclude: ["/500"],
+};
